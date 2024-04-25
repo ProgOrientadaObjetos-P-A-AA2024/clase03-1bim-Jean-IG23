2065cd997df3185a8af7954e4530a022314e98b2
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2024-04-25 16:36:41 -0500

    Update FacturaTelefono.java

--- ManejoClasesObjetos/src/paqueteuno/FacturaTelefono.java
+++ ManejoClasesObjetos/src/paqueteuno/FacturaTelefono.java
@@ -10,44 +10,52 @@ package paqueteuno;
  * @author reroes
  */
 public class FacturaTelefono {
+
+    private String nombre;
     private String numeroTelefono;
     private double minutosMes;
     private double valorMinuto;
     private double valorFactura;
-    
-    public void establecerNumeroTelefono(String c){
+
+    public void establecerNombre(String n) {
+        nombre = n;
+    }
+
+    public void establecerNumeroTelefono(String c) {
         numeroTelefono = c;
     }
-    
-    public void establecerMinutosMes(double c){
+
+    public void establecerMinutosMes(double c) {
         minutosMes = c;
     }
-    
-    public void establecerValorMinuto(double c){
+
+    public void establecerValorMinuto(double c) {
         valorMinuto = c;
     }
-    
+
     // public void establecerValorFactura(){
-    public void calcularValorFactura(){
+    public void calcularValorFactura() {
         valorFactura = minutosMes * valorMinuto;
     }
-    
-    public String obtenerNumeroTelefono(){
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public String obtenerNumeroTelefono() {
         return numeroTelefono;
     }
-    
-    public double obtenerMinutosMes(){
+
+    public double obtenerMinutosMes() {
         return minutosMes;
     }
-    
-    public double obtenerValorMinuto(){
+
+    public double obtenerValorMinuto() {
         return valorMinuto;
     }
-    
-    public double obtenerValorFactura(){
+
+    public double obtenerValorFactura() {
         return valorFactura;
     }
-    
-    
-    
+
 }
